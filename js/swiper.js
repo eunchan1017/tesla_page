d128ffefc25403c64fd4c82764d6401a3e2ce72f
eunchan1017 <jeongeunchan1017@gmail.com>
2024-10-10 13:07:15 +0900

    슬라이드 반응형 적용

--- js/swiper.js
+++ js/swiper.js
@@ -7,21 +7,21 @@ const swiper = new Swiper(".swiper", {
     effect: "fade", // slide, fade, cube, flip, coverflow
     allowTouchMove: false,
     // 캐러셀 만들기
-    slidesPerView: 3, // 보여질 슬라이드 갯수
-    spaceBetween: 20, //간격 20px
+    slidesPerView: 1, // 보여질 슬라이드 갯수
+    // spaceBetween: 20, //간격 20px
     centeredSlides: true, //첫번째 슬라이드를 가운데로
 
     //RWD setting : min-width, 모바일부터 PC까지 설정한다.
-    breakpoints: {
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
-        1200: {
-            slidesPerView: 1,
-            spaceBetween: 30,
-        },
-    },
+    // breakpoints: {
+    //     768: {
+    //         slidesPerView: 3,
+    //         spaceBetween: 30,
+    //     },
+    //     1200: {
+    //         slidesPerView: 1,
+    //         spaceBetween: 30,
+    //     },
+    // },
     // If we need pagination
     pagination: {
         el: ".swiper-pagination",
